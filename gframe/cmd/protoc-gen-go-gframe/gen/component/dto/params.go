204d79898991b079b963793f72cb9ce84d2787b4
brunowang <625653507@qq.com>
2022-09-03 16:18:19 +0800

    config generator
    config generator

---
+++ gframe/cmd/protoc-gen-go-gframe/gen/component/dto/params.go
@@ -0,0 +1,87 @@
+package dto
+
+import (
+	"fmt"
+	"github.com/brunowang/gframe/cmd/protoc-gen-go-gframe/gen/helper"
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"strings"
+)
+
+type Params struct {
+	plugin *protogen.Plugin
+	goPkg  string
+	name   string
+}
+
+func (a *Params) Setup(plugin *protogen.Plugin) {
+	a.plugin = plugin
+	a.goPkg = "dto"
+	a.name = "params"
+}
+
+func (a *Params) Generate(config helper.GenerateConfig) {
+	for _, file := range a.plugin.Files {
+		if !file.Generate {
+			continue
+		}
+		fhead := helper.NewCodeHeader().Pkg(a.goPkg).
+			Import(string(file.GoImportPath)).
+			Import("github.com/golang/protobuf/jsonpb")
+		projName := string(file.GoPackageName)
+
+		importDomain := strings.Split(string(file.GoImportPath), "/")[0]
+		fpath := fmt.Sprintf("%s/projects/%s/%s/%s.go",
+			importDomain, projName, a.goPkg, a.name)
+		g := a.plugin.NewGeneratedFile(fpath, file.GoImportPath)
+		g.P(fhead)
+		tmpl := ParamsTpl{
+			ProjName: projName,
+		}
+		reqMap := make(map[protoreflect.Name]struct{})
+		rspMap := make(map[protoreflect.Name]struct{})
+		for _, s := range file.Services {
+			for _, method := range s.Methods {
+				reqMap[method.Input.Desc.Name()] = struct{}{}
+				rspMap[method.Output.Desc.Name()] = struct{}{}
+			}
+		}
+		for _, pbmsg := range file.Messages {
+			isReq := false
+			if _, ok := reqMap[pbmsg.Desc.Name()]; ok {
+				isReq = true
+			}
+			isRsp := false
+			if _, ok := rspMap[pbmsg.Desc.Name()]; ok {
+				isRsp = true
+			}
+			msg := Message{
+				Name:  helper.ToCamelCase(string(pbmsg.Desc.Name())),
+				IsReq: isReq,
+				IsRsp: isRsp,
+			}
+			for _, field := range pbmsg.Fields {
+				defaultVal := "0"
+				switch field.Desc.Kind() {
+				case protoreflect.StringKind:
+					defaultVal = "\"\""
+				case protoreflect.MessageKind:
+					defaultVal = "nil"
+				}
+				checkLen := false
+				if field.Desc.IsList() ||
+					field.Desc.IsMap() ||
+					field.Desc.Kind() == protoreflect.BytesKind {
+					checkLen = true
+				}
+				msg.Fields = append(msg.Fields, Field{
+					Name:       field.GoName,
+					CheckLen:   checkLen,
+					DefaultVal: defaultVal,
+				})
+			}
+			tmpl.Messages = append(tmpl.Messages, msg)
+		}
+		g.P(tmpl.Render())
+	}
+}
---
+++ gframe/cmd/protoc-gen-go-gframe/gen/helper/string.go
@@ -0,0 +1,56 @@
+package helper
+
+import (
+	"regexp"
+	"strings"
+)
+
+const (
+	DeprecationComment = "// Deprecated: Do not use."
+)
+
+var matchFirstCap = regexp.MustCompile("([A-Z])([A-Z][a-z])")
+var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+func ToSnakeCase(input string) string {
+	output := matchFirstCap.ReplaceAllString(input, "${1}_${2}")
+	output = matchAllCap.ReplaceAllString(output, "${1}_${2}")
+	output = strings.ReplaceAll(output, "-", "_")
+	return strings.ToLower(output)
+}
+
+func ToCamelCase(s string) string {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return s
+	}
+
+	n := strings.Builder{}
+	n.Grow(len(s))
+	capNext := true
+	for i, v := range []byte(s) {
+		vIsCap := v >= 'A' && v <= 'Z'
+		vIsLow := v >= 'a' && v <= 'z'
+		if capNext {
+			if vIsLow {
+				v += 'A'
+				v -= 'a'
+			}
+		} else if i == 0 {
+			if vIsCap {
+				v += 'a'
+				v -= 'A'
+			}
+		}
+		if vIsCap || vIsLow {
+			n.WriteByte(v)
+			capNext = false
+		} else if vIsNum := v >= '0' && v <= '9'; vIsNum {
+			n.WriteByte(v)
+			capNext = true
+		} else {
+			capNext = v == '_' || v == ' ' || v == '-' || v == '.'
+		}
+	}
+	return n.String()
+}
